df03653076d4a07dd247a158108d6b45e249770d
gloriosomichele7-a11y <gloriosomichele7@gmail.com>
2026-07-25 16:52:11 +0200

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -7,22 +7,34 @@ return false;
 }
 
 if(!navigator.clipboard || typeof navigator.clipboard.writeText!=="function"){
-if(typeof showToast==="function"){
+
+if(typeof showMessage==="function"){
+showMessage("Copy is not supported in this browser.","error");
+}else if(typeof showToast==="function"){
 showToast("Copy is not supported in this browser.","error");
 }
+
 return false;
 }
 
 navigator.clipboard.writeText(text)
 .then(function(){
-if(typeof showToast==="function"){
+
+if(typeof showMessage==="function"){
+showMessage(successMessage,"success");
+}else if(typeof showToast==="function"){
 showToast(successMessage,"success");
 }
+
 })
 .catch(function(){
-if(typeof showToast==="function"){
+
+if(typeof showMessage==="function"){
+showMessage("Copy failed.","error");
+}else if(typeof showToast==="function"){
 showToast("Copy failed.","error");
 }
+
 });
 
 return true;
